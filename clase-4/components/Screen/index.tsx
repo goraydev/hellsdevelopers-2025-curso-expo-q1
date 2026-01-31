e2788dab2ebe9763c133180ed3b00412dcf35a03
goraydev <jofre1236@gmail.com>
2026-01-31 12:31:28 -0500

    Añadiendo más categorias en el movieapp

--- clase-4/components/Screen/index.tsx
+++ clase-4/components/Screen/index.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import React from "react";
+import { ScrollView } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
-import { Header } from '@/components/Header';
+import { Header } from "@/components/Header";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 type Props = {
-    children: React.ReactNode
-    title?: string
-    scroll?: boolean
+  children: React.ReactNode;
+  title?: string;
+  scroll?: boolean;
 };
 
 export function Screen({ children, title, scroll = true }: Props) {
-    return (
-        <SafeAreaProvider>
-            <SafeAreaView style={styles.container} edges={['top']}>
-            { title ? (<Header title={title} />) : null }
-            {scroll ? <ScrollView style={styles.scrollView}>{children}</ScrollView> : children}
-            </SafeAreaView>
-        </SafeAreaProvider>
-
-    );
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={["top"]}>
+        {title ? <Header title={title} /> : null}
+        {scroll ? (
+          <ScrollView style={styles.scrollView}>{children}</ScrollView>
+        ) : (
+          children
+        )}
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
